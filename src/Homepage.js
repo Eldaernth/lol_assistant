55ef5c1bb3674f7883ae36dca0c0dc047a8acd6b
Joó Norbert <joonorbi12@gmail.com>
2020-04-28 13:02:37 +0200

    Initial commit

---
+++ src/Homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Homepage() {
+    return(<div>
+        <div>
+        <img src="/wukong.jpeg"/>
+        <img src="/ahri.jpeg"/>
+        </div>
+        <div>
+
+        </div>
+    </div>)
+}

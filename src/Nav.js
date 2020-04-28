e4229232d5914bbe3a31e617a1e560683f874d5f
Joó Norbert <joonorbi12@gmail.com>
2020-04-28 14:00:49 +0200

    styled navbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,16 @@
 import React from "react";
+import "./Nav.css"
 
 export default function NavBar() {
     return (
-        <div>
-            <div></div>
-            <div>
+        <div className="navbar">
+            <div className="logo_wrapper">
+                <span>Lol Assistant</span>
+            </div>
+            <div className="sign_buttons">
                 <ul>
-                    <li>Sign in</li>
-                    <li>Sign up</li>
+                    <li className="sign_in">Sign in</li>
+                    <li className="sign_up">Sign up</li>
                 </ul>
             </div>
         </div>
